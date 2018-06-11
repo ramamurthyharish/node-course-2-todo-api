90eb5af06e25023a89756ce9673676a96b540eca
unknown <ramamurthyharish@gmail.com>
2018-06-11 13:41:56 +0530

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectId} = require('mongoDb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -29,6 +30,24 @@ app.get('/todos' , (req , res) => {
       res.status(400).send(e);
     });
 });
+
+
+app.get('/todos/:id' , (req , res) => {
+  var id = req.params.id ;
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch( (e) => {
+    res.status(400).send();
+  });
+
+});
+
 
 app.listen(3000 , () => {
   console.log('App is listening on port: 3000');
