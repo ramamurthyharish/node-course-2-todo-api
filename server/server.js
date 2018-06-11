5d3d325ac53ae5466f6a2771c58921c8c3872bcd
unknown <ramamurthyharish@gmail.com>
2018-06-11 11:47:39 +0530

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -11,14 +11,6 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos' , (req , res) => {
-  // var todo = new Todo({
-  //   text : req.body.text
-  // });
-  // todo.save().then((doc) => {
-  //   res.send(doc);
-  // } , (e) => {
-  //   res.status(400).send(e);
-  // })
   console.log(req.body);
   var todo = new Todo({
     text : req.body.text
@@ -30,6 +22,13 @@ app.post('/todos' , (req , res) => {
   });
 });
 
+app.get('/todos' , (req , res) => {
+    Todo.find({}).then((todos) => {
+      res.send({todos});
+    } , (e) => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(3000 , () => {
   console.log('App is listening on port: 3000');
