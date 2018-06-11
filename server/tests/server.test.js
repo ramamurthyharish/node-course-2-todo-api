b042b01c1b16ca7b91e5601bbd6bacb9b873ec25
unknown <ramamurthyharish@gmail.com>
2018-06-11 12:08:53 +0530

    Add tests fot GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,14 @@ const request = require('supertest');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
+
+
+const todos = [{text : 'First test todo '} , {text : 'Second test todo'}];
+
 beforeEach((done) => {
   Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => {
     done();
   });
 });
@@ -27,8 +33,8 @@ describe('POST /todos' , () => {
 
       }
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(1);
-        expect(todos[0].text).toBe(text);
+        expect(todos.length).toBe(3);
+        expect(todos[2].text).toBe(text);
         done();
       }).catch((e) => {
         done(e);
@@ -49,11 +55,25 @@ describe('POST /todos' , () => {
 
       }
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => {
         done(e);
       })
     })
+  });
+});
+
+
+
+describe('GET /todos' , () => {
+  it('Should display all todos' , (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done)
   });
 });
